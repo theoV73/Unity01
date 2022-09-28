0f2dc40a400aa6f98d9c439a673fea84b3ba5e41
Theo <vicini.tv@gmail.com>
2022-09-28 11:20:21 +0200

    Exo1

--- Assets/Scripts/ToolsConsols.cs
+++ Assets/Scripts/ToolsConsols.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ToolsConsols : MonoBehaviour
 {
+    [MenuItem("Tools/Log Console")]
+    static void DoSomething()
+    {
+        Debug.Log("Doing Something...");
+    }
     // Start is called before the first frame update
     void Start()
     {
